cd710532e1b6bdd1bf086f2ea8a7214caf13f073
Victor cajubá de Britto Bacelar Caldas <victorcajuba@gmail.com>
2016-07-13 21:04:04 -0300

    declaracao de algumas bibliotecas

--- Snake.c
+++ Snake.c
@@ -1,5 +1,12 @@
 #include<stdio.h>
-
+#include <stdio.h> /* Standard I/O functions */
+#include <stdlib.h> /* Miscellaneous functions (rand, malloc, srand)*/
+#include <ncurses.h> /* Screen handling and optimisation functions */
+#include <getopt.h> /* get options from system argc/argv */
+#include <string.h> /* Strings functions definitions */
+#include <assert.h> /* Verify assumptions with assert */
+#include <unistd.h> /* UNIX standard function */
+#include "tirao.h" /* 
 Int main (void)
 {
    Int a, b,c,d;
