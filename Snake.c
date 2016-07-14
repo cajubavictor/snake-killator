bd2c6c2411eb9866bb8b6c28402041b4445cd242
Victor cajubá de Britto Bacelar Caldas <victorcajuba@gmail.com>
2016-07-13 21:09:22 -0300

    laço while criado e algumas deleçoes

--- Snake.c
+++ Snake.c
@@ -1,15 +1,18 @@
 #include<stdio.h>
-#include <stdio.h> /* Standard I/O functions */
-#include <stdlib.h> /* Miscellaneous functions (rand, malloc, srand)*/
-#include <ncurses.h> /* Screen handling and optimisation functions */
-#include <getopt.h> /* get options from system argc/argv */
-#include <string.h> /* Strings functions definitions */
-#include <assert.h> /* Verify assumptions with assert */
-#include <unistd.h> /* UNIX standard function */
+#include <stdio.h> 
+#include <stdlib.h> 
+#include <ncurses.h> 
+#include <getopt.h> 
+#include <string.h> 
+#include <assert.h> 
+#include <unistd.h> 
 #include "tirao.h" /* 
 Int main (void)
 {
    Int a, b,c,d;
-    For(a =0;a <10;a++)
+     while(1)
+     {
+        
+     }
  return 0;
 }
